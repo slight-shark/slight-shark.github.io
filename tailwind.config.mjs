427edf5108ac741f0ebfb4a64eddda1412b5581a
mkofdwu <mkofdwu@gmail.com>
2025-05-13 22:06:16 +0800

    add tailwind, home page (incomplete)

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#06070a',
+        'app-yellow': '#FFC01D',
+        'app-purple': '#7231FF',
+      },
+    },
+  },
+  plugins: [],
+};
