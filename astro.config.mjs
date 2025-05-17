34ef47f0654885521e917dc91825f61f4d5a39ee
samuzora <lucastanyj@gmail.com>
2025-05-17 16:00:52 +0800

    add members page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+
+import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        black: '#06070a',
-        'app-yellow': '#FFC01D',
-        'app-purple': '#7231FF',
+        black: "#06070a",
+        "app-yellow": "#FFC01D",
+        "app-purple": "#7231FF",
+        "app-gray": "#1C1C1C",
       },
     },
   },
