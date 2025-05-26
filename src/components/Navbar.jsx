139026d38763dd08b8c007662206e9e92f0b031e
samuzora <lucastanyj@gmail.com>
2025-05-26 11:37:32 +0800

    add interactive search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,17 @@ import slightSmileFace from "../assets/slight_smile_face.svg";
 export default function Navbar() {
   const url = window.location.pathname;
   const [menuShown, showMenu] = useState(false);
+  const [searchShown, showSearch] = useState(false);
+
+  const [searchValue, setSearch] = useState("");
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // TODO: implement search logic
 
   return (
-    <div className="h-[5rem] flex items-center justify-between text-lg">
+    <div className="h-[5rem] flex items-center justify-between text-lg overflow-x-hidden">
       <div className="h-full relative flex justify-start items-center">
         <div className="ml-6 flex items-center justify-center gap-5 text-app-yellow">
           <img
@@ -45,8 +53,15 @@ export default function Navbar() {
         </div>
       </div>
       <div className="h-full w-1/4 relative flex justify-end items-center">
-        <i className="fa-regular fa-magnifying-glass bg-app-purple rounded-full aspect-square p-[1rem] mr-6">
+        <i className="fa-regular fa-magnifying-glass bg-app-purple rounded-full aspect-square p-[1rem] mr-6 cursor-pointer" onClick={() => showSearch(true)}>
         </i>
+        <div className={(searchShown ? "" : "translate-x-full") + " " + "box-border w-[100vw] lg:w-[40vw] absolute top-0 right-0 h-full rounded-l-full bg-app-purple transition-all flex px-6 items-center justify-start gap-8 text-black z-10"}>
+          <div className="mr-auto cursor-pointer">
+            <i className="fa-solid fa-x" onClick={() => showSearch(false)} />
+          </div>
+          <input className="flex-grow rounded-lg bg-app-purple text-white outline-none" placeholder="Search" value={searchValue} onChange={updateSearch}>
+          </input>
+        </div>
       </div>
     </div>
   )
