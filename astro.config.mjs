2d6e60d88a1dffb46b23bce74c2b25e6741f2c2b
samuzora <lucastanyj@gmail.com>
2025-06-09 12:44:25 +0800

    feat: add toc to writeups page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx"
 
 import yaml from "@rollup/plugin-yaml";
+import sectionize from "@hbsnow/rehype-sectionize"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
   vite: {
     plugins: [yaml()]
+  },
+  markdown: {
+    rehypePlugins: [
+      sectionize
+    ]
   }
 });
---
+++ src/components/Toc.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from "react"
+
+export default function Toc({ headings }) {
+  const tocRef = useRef(null)
+  useEffect(() => {
+    const markdownBody = document.getElementsByClassName("markdown-body")[0]
+
+    const onElementObserved = (entries) => {
+      entries.forEach(({ target, isIntersecting }) => {
+        // get the actual heading from the section element
+        const heading = target.querySelector("h1, h2, h3, h4, h5, h6")
+        if (heading) {
+          const id = heading.getAttribute("id")
+          const link = tocRef.current.querySelector(`div[data-id="${id}"]`)
+          if (link) {
+            if (isIntersecting) {
+              link.className = "text-app-yellow"
+            } else {
+              link.className = "text-white"
+            }
+          }
+        }
+      })
+    }
+
+    // todo: update the offset
+    const observer = new IntersectionObserver(onElementObserved, {})
+
+    markdownBody
+      .querySelectorAll("section")
+      .forEach((section) => observer.observe(section)
+      )
+  }, [tocRef])
+
+  return (
+    <div className="hidden md:block sticky top-24 text-xl">
+      <div className="text-white opacity-80 mb-4">Sections</div>
+      <div ref={tocRef}>
+        {headings.map(h => (
+          <div data-id={h.slug} key={h.slug}>
+            <a href={"#" + h.slug}>{h.text}</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
